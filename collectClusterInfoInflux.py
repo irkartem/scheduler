7148611dbf3c5e6ef9abf8928b736f5a62cb7b83
Artem Artemev <artemirk@gmail.com>
2017-10-09 10:05:24 +0800

    Output improved

--- collectClusterInfoInflux.py
+++ collectClusterInfoInflux.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
                         if int(vls['maxvmcount']) >= int(vls['countvm']):
                             nlimit = int(vls['countvm']) - 1
                             out = decreaseLimit(hn,vls['id'],nlimit,panel)
-                            print("Decreased {}/{} {}".format(nlimit,vls['maxvmcount'],out)
+                            print("Decreased {}/{} {}".format(nlimit,vls['maxvmcount'],out))
                     if int(vls['storageinfo'].split('.')[0]) > 90:
                         overdisk += 1
                         print("{} overdisk {}".format(vls['name'],vls['meminfo']))
